946522ada1d0db21297aac83e5a4ab818cd1f3ed
vincentLagi <vincentkerja11@gmail.com>
2025-06-04 00:15:09 +0700

    Fixing

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
     },
   ]
   const handleDownload = () => {
-    const fileUrl = "/Starlette_exe/Build.zip" 
+    const fileUrl = "/Starlette_exe/Starlette.zip" 
     const fileName = "Starlette_Data.zip"
 
     const link = document.createElement("a")
